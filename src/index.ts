5fa171d10149bafc643af2790a7d73478e607835
un7c0rn <un7c0rn@un7c0rns-MacBook-Pro.local>
2023-06-28 14:12:05 -0400

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Identity } from "./lib/Interfaces";
-export { PublicKey } from "./lib/Interfaces";
-export { ActionAuthorizer } from "./lib/Interfaces";
-export { Account } from "./lib/Interfaces";
+export type { Identity } from "./lib/Interfaces";
+export type { PublicKey } from "./lib/Interfaces";
+export type { ActionAuthorizer } from "./lib/Interfaces";
+export type { Account } from "./lib/Interfaces";
