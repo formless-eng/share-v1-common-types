4c61ec221b9ace1f2c6c24257922a01944abd993
un7c0rn <un7c0rn@un7c0rns-MacBook-Pro.local>
2023-06-28 13:31:33 -0400

    chore: update relative imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Identity } from "lib/Interfaces";
-export { PublicKey } from "lib/Interfaces";
-export { ActionAuthorizer } from "lib/Interfaces";
-export { Account } from "lib/Interfaces";
+export { Identity } from "./lib/Interfaces";
+export { PublicKey } from "./lib/Interfaces";
+export { ActionAuthorizer } from "./lib/Interfaces";
+export { Account } from "./lib/Interfaces";
