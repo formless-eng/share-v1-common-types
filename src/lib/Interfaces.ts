f331637a7812a1b963a4293ebb005e0bcb796ca2
trust-kimberly <nguyen.kimmay@gmail.com>
2023-08-14 11:10:27 -0700

    Add splits interfaces

--- src/lib/Interfaces.ts
+++ src/lib/Interfaces.ts
@@ -69,3 +69,23 @@ export interface IAsset {
     };
   };
 }
+
+export interface ISplitsData {
+  total_slots: string;
+  total_splits: number;
+  community_allocation_percent: number;
+  percent_per_slot: number;
+  splits_data: {
+    wallet_address: string;
+    unique_id: string;
+    display_name: string | null;
+    percentage: number;
+  }[];
+}
+
+export interface ISplit {
+  display_name: string | null;
+  percentage: number;
+  unique_id: string | null;
+  wallet_address: string;
+}
