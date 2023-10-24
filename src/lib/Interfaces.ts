57be5c8a1875c4ae024a1c449aa70257e4698145
trust-kimberly <nguyen.kimmay@gmail.com>
2023-10-24 10:40:59 -0700

    Add splits percentage type

--- src/lib/Interfaces.ts
+++ src/lib/Interfaces.ts
@@ -153,3 +153,8 @@ export interface IUserPreferences {
   enablePushNotifications: boolean;
   enableNotificationsEvents: NotificationEvent[];
 }
+
+export interface SplitsPercentage {
+  percentage: number;
+  settled: boolean;
+}
